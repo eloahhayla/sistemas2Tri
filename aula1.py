920be2ee7584f1c5b6056a3f0d842f55855dc676
Eloah <eloah.ferreira.silva@escola.pr.gov.br>
2025-05-16 09:26:20 -0300

    python 2 trimestre

---
+++ aula1.py
@@ -0,0 +1,13 @@
+def criaConta(agencia, numero, titular, saldo, limite, senha):
+    conta = {
+        "agencia": agencia,
+        "numero": numero,
+        "titular": titular,
+        "saldo": saldo,
+        "limite": limite,
+        "senha": senha
+    }
+    return conta
+
+conta2 = criaConta(123, "2000-2", "Márcio", 15000.0, 50000,2023)
+print(f"Agência: {conta2['agencia']}, Conta: {conta2['numero']} - SALDO: {conta2['saldo']}")
---
+++ bom saber.py
@@ -0,0 +1,3 @@
+
+nome = "Eloah"
+print(f"Olá, Mundo! Meu nome é {conta2['titular']}")
