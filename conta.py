ea0e0f0962a9ba4cea855300ff16b8a2bc81935b
Eloah <eloah.ferreira.silva@escola.pr.gov.br>
2025-05-26 12:21:23 -0300

    tela.py

--- conta.py
+++ conta.py
@@ -1,8 +1,4 @@
-conta1 = criaConta(321, "02002-0", "Hayssa", 100.0, 1000, 3202)
-print(f"Agência: {conta1['agencia']}, Conta: {conta1['numero']} - SALDO: {conta1['saldo']}") 
 
-conta2 = criaConta(123, "2000-2", "Márcio", 15000.0, 50000, 2023)
-print(f"Agência: {conta2['agencia']}, Conta: {conta2['numero']} - SALDO: {conta2['saldo']}") 
 
 class Conta:
     # método construtor
