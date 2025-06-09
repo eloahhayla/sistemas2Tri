4d04a93b033421d26bf4432e67f531f3362584e3
Eloah <eloah.ferreira.silva@escola.pr.gov.br>
2025-06-09 11:27:37 -0300

    clienye

--- tela.py
+++
@@ -1,8 +0,0 @@
-from conta import Conta
-
-conta1 = Conta(123,"10-1", "Marcinho", 5000, 1234)
-conta2 = Conta(123,"10-2", "Joaninha", 200, 1234)
-
-conta1.pix(200,conta2)
-print(conta1.extrato())
-print(conta2.extrato())
--- telaGerente.py
+++ telaGerente.py
@@ -1,17 +1,20 @@
+import random
+import getpass
+import csv
 from conta import Conta
 
-# - Criar um formulário, para o gerente preencher os dados da conta
-# - Usar prints e inputs para receber:
-# - Agência
-# - Número da conta (criar um número aleatório de 5 digitos)
-# - Titular
-# - Saldo
-# - Senha
-# - Usar laço de repetição infinito para que o sistema permita cadastrar várias contas
-while True:
-    print("*Cadastro de Conta*")
-    agencia = input("Agência: ")
-    senha = input("senha: ")
-    titular = input("Titular: ")
-    saldo = input("Saldo: ")
-    print("*Conta criada!")
+def escreveArquivo(agencia, numero, titular, saldo, senha):
+    with open('contas.csv', 'a', newline='') as csvfile:
+        escritor = csv.writer(csvfile, delimiter = ',')
+        escritor.writerow([agencia, numero, titular, saldo, senha])
+
+while(True):
+    print("### CADASTRO DE CONTA BANCÁRIA ###\nDigite os dados necessários")
+    agencia = int(input("Número da agência: "))
+    numero = random.randint(10000, 99999)
+    titular = input("Nome completo do cliente: ")
+    saldo = float(input("Saldo inicial: "))
+    senha = getpass.getpass("Solicite uma nova senha: ")
+    escreveArquivo(agencia, numero, titular, saldo, senha)
+    novaConta = Conta(agencia, numero, titular, saldo, senha)
+    print(f'Conta cadastrada com sucesso!\nNúmero da conta: {novaConta.numero}')
