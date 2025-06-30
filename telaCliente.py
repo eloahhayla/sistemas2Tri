8baeecc4e3ee6e8c2bcb5fb3644f644aac77c34f
Eloah <eloah.ferreira.silva@escola.pr.gov.br>
2025-06-30 11:04:40 -0300

    clirnte

--- telaCliente.py
+++ telaCliente.py
@@ -3,15 +3,30 @@ import csv
 from conta import Conta
 
 contas = []
-with open("contas.csv", newline = '', encoding = 'utf-8', errors = 'ignore') as lerContas:
+with open('contas.csv', newline = '', encoding = 'utf-8', errors = 'ignore') as lerContas:
     leitor = csv.reader(lerContas, delimiter = ',')
     for linha in leitor:
-        conta = Conta(linha[0], linha[1], linha[2], linha[3], linha[4])
+        conta = Conta(int(linha[0]), int(linha[1]),linha[2],float(linha[3]), linha[4])
         contas.append(conta)
-
-print(contas)
 
 
 agencia = int(input("Digite a agência: "))
-numeroconta = int(input("Digite a sua conta corrente: "))
+numero_conta = int(input("Digite sua conta corrente: "))
 senha = getpass.getpass("Digite a senha: ")
+
+# Encontrar a conta corrente na lista de contas
+contaEncontrada = None
+for conta in contas:
+    if numero_conta == conta.numero:
+        contaEncontrada = Conta(conta.agencia, conta.numero, conta.titular, conta.saldo, conta.senha)
+
+if contaEncontrada == None: # verifica se a conta é Nula (inexistente)
+    print("Dados incorretos")
+else:
+    # Verificar se o usuário digitou a senha e agência corretamente
+    if senha == contaEncontrada.senha and agencia == contaEncontrada.agencia:
+        print("Acesso liberado")
+    else:
+        print("Dados incorretos")
+
+# Criar função para verificação de login
